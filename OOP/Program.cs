bca48f9b359563227ffe11c718dbf0fef9a32a5b
mkhoyanarmen <93216230+mkhoyanarmen@users.noreply.github.com>
2023-06-23 22:30:34 +0400

    Add project files.

---
+++ OOP/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP
+{
+    class A
+    {
+        int a = 44; // initializer
+        B ob = new B(); // composition
+        B ob2; // aggregation
+        public A() // default constructor
+        {
+            Console.WriteLine("AAAAAAA");
+        }
+    }
+    class B
+    {
+        public B()
+        {
+            Console.WriteLine("BBBBBBBB");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A ob = new A();
+            //A ob2 = new A { a = 55 }; // this is initializer, when (a) variable is public
+        }
+    }
+}
